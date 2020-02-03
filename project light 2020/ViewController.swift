0e6788161826467ccb5958485bfb1e341c612ff2
Alisher <batyrkanov.ali82@gmail.com>
2020-02-03 20:17:34 +0200

    Initial Commit

---
+++ project light 2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project light 2020
+//
+//  Created by Alisher Batyrkanov on 03.02.2020.
+//  Copyright © 2020 Alisher Batyrkanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
